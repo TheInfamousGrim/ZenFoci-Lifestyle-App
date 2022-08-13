765e7d3c4ffae7771ae9403fd742f462c07410b6
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-13 18:03:59 +0100

    Added selectable time slots for the calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,10 +1,25 @@
 /* -------------------------------------------------------------------------- */
+/*                               add event modal                              */
+/* -------------------------------------------------------------------------- */
+
+// When I select a day on the mini calendar
+// I'll select the same day on the full calendar
+
+let dateSelected = dayjs().format('YYYY-MM-DD');
+
+/* -------------------------------------------------------------------------- */
 /*                      vanilla javascript mini calendar                      */
 /* -------------------------------------------------------------------------- */
 
 document.addEventListener('DOMContentLoaded', () => {
-    const calendar = new VanillaCalendar('.vanilla-calendar');
-    calendar.init();
+    const calendarMini = new VanillaCalendar('.vanilla-calendar', {
+        actions: {
+            clickDay(e, dates) {
+                dateSelected = dates[0];
+            },
+        },
+    });
+    calendarMini.init();
 });
 
 /* -------------------------------------------------------------------------- */
@@ -14,19 +29,36 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
+        initialView: 'timeGridWeek',
         height: 650,
+        // initialize selectable trait
+        selectable: true,
+        // Set up for add event button
+        customButtons: {
+            newEventButton: {
+                text: '+ add event',
+                click() {
+                    alert('clicked the custom button!');
+                },
+            },
+        },
+        // Initializes what's in the header of the calendar
         headerToolbar: {
-            start: 'title',
-            center: '',
+            start: 'newEventButton',
+            center: 'title',
             end: 'prev today next',
         },
     });
+    // Renders the calendar to the screen
     calendar.render();
+    // Updates the size to fit in the container
     calendar.updateSize();
+    // go to the date specified in the mini calendar
 });
 
 // event types
 // habits
 // goals
-//
+
+// When I select a day on the full calendar
+// The mini calendar updates as well
