1e39828b4409b7f678df953db5aa73768a81fa13
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-13 21:01:54 +0100

    added icons with tooltips and optimized further for all devices

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -8,30 +8,74 @@
 
 const calendarEl = document.getElementById('calendar');
 const calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'timeGridWeek',
-    height: 650,
-    windowResize: () => {
-        if ($(window).width() < 678) {
-            calendar.changeView('timeGridDay');
-        } else calendar.changeView('timeGridWeek');
-    },
-    // initialize selectable trait
-    selectable: true,
+    // Set the initial for depending on the window width
+    initialView: $(window).width() < 678 ? 'timeGridDay' : 'timeGridWeek',
+    height: $(window).width() < 678 ? 'auto' : 650,
+    // Theme system for icons
+    themeSystem: 'bootstrap',
     // Set up for add event button
     customButtons: {
         newEventButton: {
-            text: '+ add event',
+            hint: 'add a new event to the calendar',
             click() {
+                /* ---------------- CHANGE!!!! THIS IS JUST A PLACEHOLDER!!! ---------------- */
                 alert('clicked the custom button!');
             },
         },
+        dayCalendarButton: {
+            hint: 'view the calendar by the selected day',
+            click() {
+                alert('day calendar has been clicked');
+            },
+        },
+        weekCalendarButton: {
+            hint: 'view the calendar by the selected week',
+            click() {
+                alert('week calendar has been clicked');
+            },
+        },
     },
-    // Initializes what's in the header of the calendar
-    headerToolbar: {
-        start: 'newEventButton',
-        center: 'title',
-        end: 'prev today next',
+    // Set up for button icons
+    bootstrapFontAwesome: {
+        newEventButton: 'fa-solid fa-calendar-circle-plus',
+        dayCalendarButton: 'fa-solid fa-calendar-day',
+        weekCalendarButton: 'fa-solid fa-calendar-week',
     },
+    // Change the view and toolbar depending on window width being resized
+    windowResize: () => {
+        if ($(window).width() < 678) {
+            calendar.changeView('timeGridDay');
+            calendar.setOption('height', 'auto');
+            calendar.setOption('headerToolbar', {
+                start: 'prev',
+                center: 'title',
+                end: 'next',
+            });
+        } else {
+            calendar.changeView('timeGridWeek');
+            calendar.setOption('height', 650);
+            calendar.setOption('headerToolbar', {
+                start: 'newEventButton',
+                center: 'title',
+                end: 'dayCalendarButton weekCalendarButton prev today next',
+            });
+        }
+    },
+    // initialize selectable trait
+    selectable: true,
+    // Initializes what's in the header of the calendar depending on screen width
+    headerToolbar:
+        $(window).width() < 678
+            ? {
+                  start: 'prev',
+                  center: 'title',
+                  end: 'next',
+              }
+            : {
+                  start: 'newEventButton',
+                  center: 'title',
+                  end: 'dayCalendarButton weekCalendarButton prev today next',
+              },
 });
 // Renders the calendar to the screen
 calendar.render();
@@ -48,7 +92,7 @@ calendar.updateSize();
 const calendarMini = new VanillaCalendar('.vanilla-calendar', {
     actions: {
         clickDay(e, dates) {
-            // go to the date selected
+            // go to the date selected on the full calendar
             calendar.gotoDate(dates[0]);
         },
     },
