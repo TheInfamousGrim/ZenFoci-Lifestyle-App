78aea14f284c53d63a5bb8f44b7544b4443ec024
ayaneey <ayan.h18@hotmail.com>
2022-08-13 21:48:55 +0100

    created a form

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,4 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const list_el = document.querySelector("#tasks");
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const task = input.value;
-
-    if (!task) {
-      alert("Please add a task");
-    } else {
-      console.log("Success");
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const elems = document.querySelectorAll(".modal");
+  const instances = M.Modal.init(elems, { opacity: 1 });
 });
