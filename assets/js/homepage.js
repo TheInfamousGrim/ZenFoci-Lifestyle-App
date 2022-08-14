0e59be789349bedbf55349736d11268044f03777
ayaneey <ayan.h18@hotmail.com>
2022-08-14 18:57:43 +0100

    created a modal to user to input habits

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,3 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const elems = document.querySelectorAll(".modal");
   const instances = M.Modal.init(elems, { opacity: 1 });
 });
+
+     
+document.addEventListener("DOMContentLoaded", function () {
+
+  const select = document.querySelector('select');
+  M.FormSelect.init(select)
+
+})
+
+
+// Dropdown initializer
+document.addEventListener('DOMContentLoaded', () => {
+  const elems = document.querySelectorAll('.dropdown-trigger');
+  const instances = M.Dropdown.init(elems, {});
+});
+
+// Select initializer
+document.addEventListener('DOMContentLoaded', () => {
+  const elems = document.querySelectorAll('select');
+  const instances = M.FormSelect.init(elems, {
+      dropdownOptions: {},
+  });
+});
+    
