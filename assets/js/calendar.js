6014f2de7bafa17c9fe626a29695d2fd25843082
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-14 21:20:25 +0100

    started messing about with event objects

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,76 +1,6 @@
 /* -------------------------------------------------------------------------- */
-/*                               new event modal                              */
+/*                                   plugins                                  */
 /* -------------------------------------------------------------------------- */
-
-// Modal initializer
-const elems = document.querySelectorAll('.modal');
-const instances = M.Modal.init(elems, {
-    startingTop: '0%',
-    endingTop: '0%',
-});
-
-// Select initializer
-document.addEventListener('DOMContentLoaded', () => {
-    const elems = document.querySelectorAll('select');
-    const instances = M.FormSelect.init(elems, {
-        dropdownOptions: {},
-    });
-});
-
-// Dropdown initializer
-document.addEventListener('DOMContentLoaded', () => {
-    const elems = document.querySelectorAll('.dropdown-trigger');
-    const instances = M.Dropdown.init(elems, {});
-});
-
-/* ------------------------------- date picker ------------------------------ */
-// selector for the date picker input container
-const dateInputPicker = $('.date-picker-input-container');
-// selector for the date input label
-const endDateInput = $('[name="end-date-input"]');
-endDateInput.val('pepega');
-
-// selector for the
-const datePickers = $('.date-picker-general');
-// add a calendar for each date-picker div
-$.each(datePickers, (key, datePicker) => {
-    const datePickerCalendar = new VanillaCalendar(datePicker, {
-        type: 'default',
-        actions: {
-            clickDay(e, dates) {
-                if (dates) {
-                    // Get the correct input
-                    const currentDateInput =
-                        e.target.parentElement.parentElement.parentElement.parentElement.parentElement
-                            .firstElementChild;
-                    // Format the date for the input
-                    const formattedDateInput = dayjs(dates[0]).format('MMMM DD, YYYY');
-                    // Add the formatted date to the selected input
-                    currentDateInput.value = formattedDateInput;
-                }
-            },
-        },
-    });
-    datePickerCalendar.init();
-});
-
-function handleDateInput(e) {
-    // select the correct date picker div
-    const currentDatePicker = e.currentTarget.lastElementChild;
-    // display the selected date picker
-    currentDatePicker.classList.remove('date-picker-general-hidden');
-    // style the selected date picker
-    currentDatePicker.classList.add('date-picker-general-visible');
-    console.log('is the input not selected: ', e.target !== document.activeElement);
-    // remove the date picker after the user has 
-    console.log('Is the date picker not being focused on: ', !currentDatePicker.contains(e.target));
-    if (!currentDatePicker.contains(e.target) && e.target !== document.activeElement) {
-        currentDatePicker.classList.remove('date-picker-general-visible');
-        currentDatePicker.classList.add('date-picker-general-hidden');
-    }
-}
-
-dateInputPicker.on('click', handleDateInput);
 
 /* -------------------------------------------------------------------------- */
 /*                         full calendar main calendar                        */
@@ -84,6 +14,7 @@ const calendar = new FullCalendar.Calendar(calendarEl, {
     height: $(window).width() < 678 ? 'auto' : 650,
     // Theme system for icons
     themeSystem: 'bootstrap',
+    nowIndicator: true,
     // Set up for add event button
     customButtons: {
         newEventButton: {
@@ -145,11 +76,65 @@ const calendar = new FullCalendar.Calendar(calendarEl, {
                   start: 'title',
                   end: 'dayCalendarButton weekCalendarButton prev today next',
               },
+    events: [
+        // General event
+        {
+            // this object will be "parsed" into an event object}
+            id: 1, // increment by one for each event so it has a unique id
+            title: 'Meet my friends',
+            // This is the event type
+            eventType: 'general',
+            classList: 'events general-events',
+            color: '#26a69a',
+            allDay: true,
+            start: '2022-08-16',
+            // startTime: '10:00',
+            end: '2022-08-16',
+            // endTime: '11:00',
+            // [sun, mon, tue, wed, thu, fri, sat]
+            // [0  , 1  , 2  , 3  , 4  , 5  , 6  ]
+            // If recurring the start or stop properties aren't needed
+            description: 'Going to a tame impala gig at the O2 venue',
+        },
+    ],
 });
 // Renders the calendar to the screen
 calendar.render();
 // Updates the size to fit in the container
 calendar.updateSize();
+
+/* ------------------------ submit the calendar form ------------------------ */
+
+console.log(dayjs.extend(utc));
+console.log(dayjs.extend(timezone));
+
+// event object constructor full calendar
+const eventsObj = {
+    events: [
+        // General event
+        {
+            // this object will be "parsed" into an event object}
+            id: 1, // increment by one for each event so it has a unique id
+            title: 'Meet my friends',
+            // This is the event type
+            groupId: 'general',
+            classList: 'events general-events',
+            color: '#26a69a',
+            allDay: false,
+            start: '2022-08-16',
+            startTime: '10:00',
+            end: '2022-08-16',
+            endTime: '11:00',
+            // [sun, mon, tue, wed, thu, fri, sat]
+            // [0  , 1  , 2  , 3  , 4  , 5  , 6  ]
+            // If recurring the start or stop properties aren't needed
+            daysOfWeek: [2],
+            description: 'Going to a tame impala gig at the O2 venue',
+        },
+    ],
+};
+
+calendar.addEvent(eventsObj);
 
 // When I select a day on the full calendar
 // The mini calendar updates as well
@@ -168,4 +153,72 @@ const calendarMini = new VanillaCalendar('.vanilla-calendar', {
 });
 calendarMini.init();
 
-const largeCalendarContainer = document.querySelector('.large-calendar-container');
+/* -------------------------------------------------------------------------- */
+/*                               new event modal                              */
+/* -------------------------------------------------------------------------- */
+
+// Modal initializer
+const elems = document.querySelectorAll('.modal');
+const instances = M.Modal.init(elems, {
+    startingTop: '0%',
+    endingTop: '0%',
+});
+
+// Select initializer
+document.addEventListener('DOMContentLoaded', () => {
+    const elems = document.querySelectorAll('select');
+    const instances = M.FormSelect.init(elems, {
+        dropdownOptions: {},
+    });
+});
+
+// Dropdown initializer
+document.addEventListener('DOMContentLoaded', () => {
+    const elems = document.querySelectorAll('.dropdown-trigger');
+    const instances = M.Dropdown.init(elems, {});
+});
+
+/* ------------------------------- date picker ------------------------------ */
+
+// selector for the date picker
+const datePickers = $('.date-picker-general');
+// add a calendar for each date-picker div
+$.each(datePickers, (key, datePicker) => {
+    const datePickerCalendar = new VanillaCalendar(datePicker, {
+        type: 'default',
+        actions: {
+            clickDay(e, dates) {
+                if (dates) {
+                    // Get the correct input
+                    const currentDateInput =
+                        e.target.parentElement.parentElement.parentElement.parentElement.parentElement
+                            .firstElementChild;
+                    // Format the date for the input
+                    const formattedDateInput = dayjs(dates[0]).format('MMMM DD, YYYY');
+                    // Add the formatted date to the selected input
+                    currentDateInput.value = formattedDateInput;
+                }
+            },
+        },
+    });
+    datePickerCalendar.init();
+});
+
+function handleDateInput(e) {
+    // select the correct date picker div
+    const currentDatePicker = e.currentTarget.lastElementChild;
+    // display the selected date picker
+    currentDatePicker.classList.remove('date-picker-general-hidden');
+    // style the selected date picker
+    currentDatePicker.classList.add('date-picker-general-visible');
+    // remove the date picker after the user has selected a date
+    if (!currentDatePicker.contains(e.target) && e.target !== document.activeElement) {
+        currentDatePicker.classList.remove('date-picker-general-visible');
+        currentDatePicker.classList.add('date-picker-general-hidden');
+    }
+}
+
+// selector for the date picker input container
+const dateInputPicker = $('.date-picker-input-container');
+// event listeners for the date inputs
+dateInputPicker.on('click', handleDateInput);
