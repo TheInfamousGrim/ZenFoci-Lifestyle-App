eef66c20b5a1fc2d8fc871c85b9aa670fa610f8c
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-17 05:38:49 +0100

    refactored darren's code and added a preLoader for the page

--- assets/js/mealSearch.js
+++ assets/js/mealSearch.js
@@ -1,29 +1,60 @@
 const button = document.querySelector('.submit');
 const input = document.querySelector('.input_text');
 const listings = document.querySelector('.list-group');
-var names = [' '];
+let names = [' '];
 const preExistingData = localStorage.getItem('shopping-list');
 const recipe = ["'<li>" + "</li>'"];
 names.push(preExistingData);
 
 if (preExistingData === null) {
-    var names = [' '];
-}
-
-function openTab(evt, id) {
-    alert(id);
-    alert(evt);
+    names = [' '];
 }
 
 button.addEventListener('click' || 'keypress', (event) => {
     event.preventDefault();
-    $('.title').empty().append(`<h2>Search results for : ${input.value}</h2>`);
     search(event);
 });
 
+/* -------------------------------------------------------------------------- */
+/*                              loader functions                              */
+/* -------------------------------------------------------------------------- */
+
+// loader container selector
+const loaderContainer = document.querySelector('.loader-container');
+
+// loader HTML
+const preLoaderHTML = `
+<div class="lds-heart center">
+    <div></div>
+</div>
+`;
+
+function displayLoader() {
+    loaderContainer.innerHTML = preLoaderHTML;
+}
+
+function removeLoader() {
+    loaderContainer.innerHTML = '';
+}
+
+/* -------------------------------------------------------------------------- */
+/*                          filter out all junk data                          */
+/* -------------------------------------------------------------------------- */
+
+function filterUndefinedData(data) {
+    const noUndefinedData = data.filter((dataItem) => dataItem !== undefined);
+    const filteredInstructionsData = data.filter(
+        (dataItem) => dataItem.instructions !== undefined && dataItem.instructions !== null
+    );
+    const filteredUserRatingData = filteredInstructionsData.filter((result) => result.user_ratings !== undefined);
+    return filteredUserRatingData;
+}
+
+/* ----------------------------- get saved data ----------------------------- */
+
 function search(event) {
     const term = input.value;
-
+    displayLoader();
     fetch(`https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=${term}`, {
         method: 'GET',
         headers: {
@@ -35,41 +66,58 @@ function search(event) {
         .then((data) => {
             const items = [];
 
+            // set innerHTML to be an empty string
+            const mealContainer = document.querySelector('.title');
+            mealContainer.innerHTML = '';
+            console.log(mealContainer.innerHTML);
+
+            // iterate through the data
             for (let i = 0; i < data.results.length; i++) {
+                // filter data out and remove the data with undefined instructions
+                const filteredData = filterUndefinedData(data.results);
+                if (filteredData[i] === undefined) {
+                    return;
+                }
+
+                // create an empty html element
                 let html = '';
 
-                const did = data.results[i].id;
-                var mealName = data.results[i].name;
-                const { description } = data.results[i];
+                const did = filteredData[i].id;
+                const mealName = filteredData[i].name;
+                const { description } = filteredData[i];
 
                 let instructionsHtml = '';
                 let ingredientsHtml = '';
                 let componentsHtml = '';
-                var nutritionHTML = `<li>Calories: ${data.results[i].nutrition.calories}<li><li>Carbohydrates: ${data.results[i].nutrition.carbohydrates}<li>`;
+                let nutritionHTML = '';
+                if (filteredData[i].nutrition !== undefined) {
+                    nutritionHTML = `<li>Calories: ${filteredData[i].nutrition.calories}<li><li>Carbohydrates: ${filteredData[i].nutrition.carbohydrates}<li>`;
+                } else {
+                    nutritionHTML = '';
+                }
 
-                if (data.results[i].instructions.length > 0) {
+                if (filteredData[i] !== undefined && filteredData[i].instructions.length > 0) {
                     instructionsHtml += '<ul>';
 
-                    for (let j = 1; j < data.results[i].instructions.length; j++) {
-                        instructionsHtml += `<li>${data.results[i].instructions[j].display_text}</li>`;
+                    for (let j = 1; j < filteredData[i].instructions.length; j++) {
+                        instructionsHtml += `<li>${filteredData[i].instructions[j].display_text}</li>`;
                     }
                     instructionsHtml += '</ul>';
 
-                    for (let k = 0; k < data.results[i].sections.length; k++) {
-                        componentsHtml += `<li>${data.results[i].sections[k]}</li>`;
+                    for (let k = 0; k < filteredData[i].sections.length; k++) {
+                        componentsHtml += `<li>${filteredData[i].sections[k]}</li>`;
                     }
                     componentsHtml += '</ul>';
 
-                    for (let l = 0; l < data.results[i].sections['0'].components.length; l++) {
-                        ingredientsHtml += `<li a href='#' class='listitem'>${data.results[i].sections['0'].components[l].raw_text}</li>`;
-                        console.log(`#tabcon-${did} a`);
+                    for (let l = 0; l < filteredData[i].sections['0'].components.length; l++) {
+                        ingredientsHtml += `<li a href='#' class='listitem'>${filteredData[i].sections['0'].components[l].raw_text}</li>`;
                     }
 
                     ingredientsHtml += '</ul>';
                 }
 
-                $('.listitem').on('click', function (event) {
-                    event.preventDefault();
+                $('.listitem').on('click', function (e) {
+                    e.preventDefault();
                     // var items = new Array();
                     // localStorage.items("items" ,$( this ).text().replace(/\d+/g, '').replace(/tablespoons|tablespoon|cups|pints|teaspoons|to taste|slices|of|ounces|sliced|teaspoon|cup|sharp|¼|¾|½|⅓|room temperature|/g,""))
                     // setItem(keyName, keyValue)
@@ -87,44 +135,41 @@ function search(event) {
 
                     const filteredNames = [...new Set(names)];
 
-                    console.log(filteredNames);
-
                     localStorage.setItem('shopping-list', JSON.stringify(filteredNames));
 
                     // console.log( $( this ).html().replace(/\d+/g, '').replace(/tablespoons|tablespoon|cups|pints|teaspoons|to taste|slices|of|ounces|sliced|teaspoon|cup|sharp|¼|¾|½|⅓|room temperature|/g,"") );
                     // console.log(names)
                 });
 
-                $('#savebutton').on('click', function (event) {
-                    event.preventDefault();
+                $('#savebutton').on('click', function (e) {
+                    e.preventDefault();
                     recipe.push(`<li>${$(this).text()}${mealName}</li>`);
                     localStorage.setItem('meal-list', JSON.stringify(recipe));
-
-                    console.log(recipe);
                 });
                 // let ratingDec = data.results[i].user_ratings;
                 // ratingDec = ratingDec * 100;
-                const rating = Math.round(data.results[i].user_ratings.score * 10);
-                const votes = data.results[i].user_ratings.count_positive + data.results[i].user_ratings.count_negative;
 
-                console.log(votes);
+                const rating = Math.round(filteredData[i].user_ratings.score * 10);
 
-                var nutritionHTML = `<li>Calories: ${data.results[i].nutrition.calories}</li><li>Carbohydrates: ${data.results[i].nutrition.carbohydrates}</li><li>Fat: ${data.results[i].nutrition.fat}</li><li>Fibre: ${data.results[i].nutrition.fibre}</li><li>Protein: ${data.results[i].nutrition.protein}</li><li>Sugar: ${data.results[i].nutrition.sugar}</li>`;
+                const votes = filteredData[i].user_ratings.count_positive + filteredData[i].user_ratings.count_negative;
+
+                nutritionHTML = `<li>Calories: ${filteredData[i].nutrition.calories}</li><li>Carbohydrates: ${filteredData[i].nutrition.carbohydrates}</li><li>Fat: ${filteredData[i].nutrition.fat}</li><li>Fibre: ${filteredData[i].nutrition.fibre}</li><li>Protein: ${filteredData[i].nutrition.protein}</li><li>Sugar: ${filteredData[i].nutrition.sugar}</li>`;
 
                 html += `<div class='tab' id='${did}'>`;
-                html += `<div class='header'><div><h2>${data.results[i].name}</h2></div><strong><p class='cooking-time'>Cooking Time: </strong>${data.results[i].cook_time_minutes}mins  -<strong>  Prep Time: </strong>${data.results[i].prep_time_minutes}mins  -<strong>  Servings: </strong>${data.results[i].num_servings}  -<strong>  User Rating: </strong>${rating}/10 (${votes} votes)</p></div><button class='btn submit' id='savebutton' value='${did}' style='float: right;' >Save Recipe</button>`;
+                html += `<div class='header'><div><h2>${filteredData[i].name}</h2></div><strong><p class='cooking-time'>Cooking Time: </strong>${filteredData[i].cook_time_minutes}mins  -<strong>  Prep Time: </strong>${filteredData[i].prep_time_minutes}mins  -<strong>  Servings: </strong>${filteredData[i].num_servings}  -<strong>  User Rating: </strong>${rating}/10 (${votes} votes)</p></div><button class='btn submit' id='savebutton' value='${did}' style='float: right;' >Save Recipe</button>`;
                 html += `<button class='tablinks tabbtn-${did} active' id='overview${did}' data-id='${did}'>Overview</button>`;
 
                 html += `<button class='tablinks tabbtn-${did}' id='ingredients${did}'  data-id='${did}'>Ingredients</button>`;
                 html += `<button class='tablinks tabbtn-${did}' id='instructions${did}'  data-id='${did}'>Instructions</button>`;
                 html += '</div>';
-                html += `<div class='tabcontent tabcon-${did} active overview${did}'><div class='row'><div class='col xl4 s12'><img class='activator' src='${data.results[i].thumbnail_url}'/></div>`;
-                html += `<div class='col xl7 s12'>${data.results[i].description}<div><h4>Nutrition</h4>${nutritionHTML}</div></div></div>`;
+                html += `<div class='tabcontent tabcon-${did} active overview${did}'><div class='row'><div class='col xl4 s12'><img class='activator' src='${filteredData[i].thumbnail_url}'/></div>`;
+                html += `<div class='col xl7 s12'>${filteredData[i].description}<div><h4>Nutrition</h4>${nutritionHTML}</div></div></div>`;
                 html += '</div>';
                 html += `<div class='tabcontent tabcon-${did} ingredients${did}'><ul class='inglist'>${ingredientsHtml}</ul><a href='#'><button class='btn btn-primary' id='addtolist'>Save</button></a></div>`;
                 html += `<div class='tabcontent tabcon-${did} instructions${did}'>${instructionsHtml}</div>`;
 
                 $('.title').append(html);
+                removeLoader();
             }
         });
     input.value = '';
