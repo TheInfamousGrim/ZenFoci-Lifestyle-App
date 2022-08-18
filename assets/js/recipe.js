6a7b3b2e3acbd5c7cfa2fdea19639defed248be4
TheInfamousGrim <finchergeorge1@gmail.com>
2022-08-18 05:51:17 +0100

    added the search history card remember to style and add a delete button later

---
+++ assets/js/recipe.js
@@ -0,0 +1,95 @@
+/* -------------------------------------------------------------------------- */
+/*                    function for getting the recipe list                    */
+/* -------------------------------------------------------------------------- */
+
+function getRecipeList() {
+    const recipeList = localStorage
+        .getItem('meal-list')
+        .replace(/-/g, '')
+        .replace(/","/g, '')
+        .replace(/[\[\]']+/g, '')
+        .replace(/["]/g, '')
+        .replace(/\\ /g, '');
+    recipeList.replaceAll('[\\]', '');
+    console.log(recipeList);
+    const preExistingMealData = localStorage.getItem('shopping-list') || [];
+
+    let mealNames = [];
+    mealNames.push(preExistingMealData);
+    if (preExistingMealData === null) {
+        mealNames = [];
+    }
+    console.log(recipeList);
+    $('#display').append(recipeList).html();
+    const savedMealsList = $('.display');
+    savedMealsList.children().addClass('collection-item');
+}
+
+getRecipeList();
+
+/* -------------------------------------------------------------------------- */
+/*                           display a saved search                           */
+/* -------------------------------------------------------------------------- */
+
+$('.item').on('click', function (event) {
+    const query = $(this).attr('id');
+    fetch(`https://tasty.p.rapidapi.com/recipes/get-more-info?id=${query}`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4d71919945mshfeeca5d2011614bp19d595jsncca0579a2980',
+            'X-RapidAPI-Host': 'tasty.p.rapidapi.com',
+        },
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log($('.title').html() !== '');
+            $('.title').html('');
+
+            let html = '';
+
+            const did = data.id;
+            const mealName = data.name;
+            const { description } = data;
+
+            let instructionsHtml = '';
+            let ingredientsHtml = '';
+            let componentsHtml = '';
+            let nutritionHTML = `<li>Calories: ${data.nutrition.calories}<li><li>Carbohydrates: ${data.nutrition.carbohydrates}<li>`;
+
+            for (let j = 1; j < data.instructions.length; j++) {
+                instructionsHtml += `<li>${data.instructions[j].display_text}</li>`;
+            }
+            instructionsHtml += '</ul>';
+
+            for (let k = 0; k < data.sections.length; k++) {
+                componentsHtml += `<li>${data.sections[k]}</li>`;
+            }
+            componentsHtml += '</ul>';
+
+            for (let l = 0; l < data.sections['0'].components.length; l++) {
+                ingredientsHtml += `<li a href='#' class='listitem'>${data.sections['0'].components[l].raw_text}</li>`;
+            }
+
+            ingredientsHtml += '</ul>';
+
+            const rating = Math.round(data.user_ratings.score * 10);
+            const votes = data.user_ratings.count_positive + data.user_ratings.count_negative;
+
+            nutritionHTML = `<li>Calories: ${data.nutrition.calories}</li><li>Carbohydrates: ${data.nutrition.carbohydrates}</li><li>Fat: ${data.nutrition.fat}</li><li>Fibre: ${data.nutrition.fibre}</li><li>Protein: ${data.nutrition.protein}</li><li>Sugar: ${data.nutrition.sugar}</li>`;
+
+            html += `<div class='tab' id='${did}'>`;
+            html += `<div class='header'><div><h2>${data.name}</h2></div><strong><p class='cooking-time'>Cooking Time: </strong>${data.cook_time_minutes}mins  -<strong>  Prep Time: </strong>${data.prep_time_minutes}mins<br><strong>  Servings: </strong>${data.num_servings}  -<strong>  User Rating: </strong>${rating}/10 (${votes} votes)</p></div>`;
+            html += `<button class='tablinks tabbtn-${did} active' id='overview${did}' data-id='${did}'>Overview</button>`;
+
+            html += `<button class='tablinks tabbtn-${did}' id='ingredients${did}'  data-id='${did}'>Ingredients</button>`;
+            html += `<button class='tablinks tabbtn-${did}' id='instructions${did}'  data-id='${did}'>Instructions</button>`;
+            html += '</div>';
+            html += `<div class='tabcontent tabcon-${did} active overview${did}'><div class='row'><div class='col s3'><img class='activator' src='${data.thumbnail_url}'/></div>`;
+            html += `<div class='col s3'>${data.description}<div><h4>Nutrition</h4>${nutritionHTML}</div></div></div>`;
+            html += '</div>';
+            html += `<div class='tabcontent tabcon-${did} ingredients${did}'><ul class='inglist'>${ingredientsHtml}</ul><a href='#'></a></div>`;
+            html += `<div class='tabcontent tabcon-${did} instructions${did}'>${instructionsHtml}</div>`;
+
+            $('.title').append(html);
+        });
+});
